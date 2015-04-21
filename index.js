e20bcd5f455b7693e5782d2d87d10074b82aca64
Anne Lin <alin0@walmartlabs.com>
2015-04-21 13:57:46 -0700

    use through

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 let http = require('http')
 let fs = require('fs')
 let request = require('request')
+let through = require('through')
 let argv = require('yargs')
-	.default({host: '127.0.0.1', port: '8000', url: '127.0.0.1:8000', log: 'proxy-server.log'})
+	.default({host: '127.0.0.1'})
     .argv   
+//	.default({host: '127.0.0.1', port: '8000', url: '127.0.0.1:8000', log: 'proxy-server.log'})
 let scheme = 'http://'
 let port = argv.port || argv.host === '127.0.0.1' ? '8000' : '80'
 let destinationUrl = argv.url || scheme + argv.host + ':' + port
 let logStream = argv.log ? fs.createWriteStream(argv.log) : process.stdout
 
 http.createServer((req, res) => {
-    logStream.write('\nRequest received at: ' + req.url)
+    logStream.write('\nEcho request: \n' + JSON.stringify(req.headers) + '\n')
     for (let header in req.headers) {
 	    res.setHeader(header, req.headers[header])
 	}
-	logStream.write('\n' + JSON.stringify(req.headers) + '\n')
-	req.pipe(logStream)
+    through(req, logStream, {autoDestroy: false})
     req.pipe(res)
-}).listen(8000);
+ }).listen(8000);
 
+logStream.write("listening at http://127.0.0.1:8001")
 
 http.createServer((req, res) => {
-	logStream.write('\nProxying request to: ' + destinationUrl + req.url)
 	destinationUrl = req.headers['x-desination-url'] || destinationUrl
 	let options = {
 		method: req.method,
@@ -29,10 +30,13 @@ http.createServer((req, res) => {
     	 url: `http://${destinationUrl}${req.url}`
 	}
 
+	logStream.write('\nProxying request to: ' + destinationUrl + req.url + '\n')
+    through(req, logStream, {autoDestroy: false})
+
 	let downstreamResponse = req.pipe(request(options))
-	logStream.write('\nProxying request:\n:' + JSON.stringify(downstreamResponse.headers))
-	//downstreamResponse.pipe(logStream)
+	logStream.write(JSON.stringify(downstreamResponse.headers))
 	downstreamResponse.pipe(res)
+    through(downstreamResponse, logStream, {autoDestroy: false})
 
 }).listen(8001);
 
