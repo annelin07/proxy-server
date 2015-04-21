83f6926f5d90c00a93c0782aa86ad21b726dcbbf
Anne Lin <alin0@walmartlabs.com>
2015-04-21 11:42:11 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+let http = require('http')
+let fs = require('fs')
+let request = require('request')
+let argv = require('yargs')
+	.default({host: '127.0.0.1', port: '8000', url: '127.0.0.1:8000', log: 'proxy-server.log'})
+    .argv   
+console.log(argv);    
+let scheme = 'http://'
+let port = argv.port || argv.host === '127.0.0.1' ? '8000' : '80'
+let destinationUrl = argv.url || scheme + argv.host + ':' + port
+let logStream = argv.log ? fs.createWriteStream(argv.log) : process.stdout
+ logStream.write('\n' + logStream instanceof WriteStream);
+
+console.log(logStream);
+http.createServer((req, res) => {
+    logStream.write('\nRequest received at: ' + req.url)
+    for (let header in req.headers) {
+	    res.setHeader(header, req.headers[header])
+	}
+	logStream.write('\n' + JSON.stringify(req.headers) + '\n')
+	req.pipe(logStream)
+    req.pipe(res)
+}).listen(8000);
+
+
+http.createServer((req, res) => {
+	logStream.write('\nProxying request to: ' + destinationUrl + req.url)
+	destinationUrl = req.headers['x-desination-url'] || destinationUrl
+	let options = {
+		method: req.method,
+    	headers: req.headers,
+    	 url: `http://${destinationUrl}${req.url}`
+	}
+
+	let downstreamResponse = req.pipe(request(options))
+	logStream.write('\nProxying request:\n:' + JSON.stringify(downstreamResponse.headers))
+	//downstreamResponse.pipe(logStream)
+	downstreamResponse.pipe(res)
+
+}).listen(8001);
+
+
+
+function logMessage(level, stream) {
+    // Your code here
+}
+
+function logMessage(level, message) {
+    // Your code here
+}
