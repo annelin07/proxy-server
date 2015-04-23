5f7f954340f5bdd6703c002661d02755c4f5fe2b
Anne Lin <alin0@walmartlabs.com>
2015-04-23 13:42:20 -0700

    Add --exec support.

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Time spent: [8hrs, including setting up environment, coding, testing, recording 
 * [done] Required: The destination server is configurable via the `x-destination-url` header
 * [done] Required: Client requests and respones are printed to stdout
 * [done] Required: The `--logfile` argument outputs all logs to the file specified instead of stdout
-* [todo] Optional: The `--exec` argument proxies stdin/stdout to/from the destination program
+* [done] Optional: The `--exec` argument proxies stdin/stdout to/from the destination program
 * [todo] Optional: The `--loglevel` argument sets the logging chattiness
 * [todo] Optional: Supports HTTPS
 * [todo] Optional: `-h` argument prints CLI API
--- index.js
+++ index.js
@@ -1,44 +1,55 @@
-let http = require('http')
 let fs = require('fs')
-let request = require('request')
-let through = require('through')
 let argv = require('yargs')
 	.default({host: '127.0.0.1'})
     .argv   
-let scheme = 'http://'
-let port = argv.port || argv.host === '127.0.0.1' ? '8000' : '80'
-let destinationUrl = argv.url || scheme + argv.host + ':' + port
-let logStream = argv.lf ? fs.createWriteStream(argv.lf) : process.stdout
-// logStream.write(JSON.stringify(argv))
-// logStream.write(destinationUrl)
 
-http.createServer((req, res) => {
-    logStream.write('\nEcho request: \n' + JSON.stringify(req.headers) + '\n')
-    for (let header in req.headers) {
-	    res.setHeader(header, req.headers[header])
-	}
-    through(req, logStream, {autoDestroy: false})
-    req.pipe(res)
- }).listen(8000);
+if (argv.exec) {
+	// exec using child process
+	let spawn = require('child_process').spawn;
+	let argvExec = argv.exec.split(' ');
+	let command = argvExec[0];
+	let args = argvExec.splice(1,1);
+	// spawn child process which uses parent's stdios (stdin, stdout, stderr)
+	let child = spawn(command, args, {stdio: 'inherit'});
 
-logStream.write("\nlistening at http://127.0.0.1:8001\n")
+} else {
+	// start servers
+	let http = require('http')
+	let request = require('request')
+	let through = require('through')
 
-http.createServer((req, res) => {
-	destinationUrl = req.headers['x-desination-url'] || destinationUrl
-	let options = {
-		method: req.method,
-    	headers: req.headers,
-    	 url: `http://${destinationUrl}${req.url}`
-	}
+	let scheme = 'http://'
+	let port = argv.port || argv.host === '127.0.0.1' ? '8000' : '80'
+	let destinationUrl = argv.url || scheme + argv.host + ':' + port
+	let logStream = argv.lf ? fs.createWriteStream(argv.lf) : process.stdout
 
-	logStream.write('\nProxying request to: ' + destinationUrl + req.url + '\n')
-    through(req, logStream, {autoDestroy: false})
+	http.createServer((req, res) => {
+	    logStream.write('\nEcho request: \n' + JSON.stringify(req.headers) + '\n')
+	    for (let header in req.headers) {
+		    res.setHeader(header, req.headers[header])
+		}
+	    through(req, logStream, {autoDestroy: false})
+	    req.pipe(res)
+	 }).listen(8000);
 
-	let downstreamResponse = req.pipe(request(options))
-	logStream.write('\n' + JSON.stringify(downstreamResponse.headers) + '\n')
-	downstreamResponse.pipe(res)
-    through(downstreamResponse, logStream, {autoDestroy: false})
+	logStream.write("\nlistening at http://127.0.0.1:8001\n")
 
-}).listen(8001);
+	http.createServer((req, res) => {
+		destinationUrl = req.headers['x-desination-url'] || destinationUrl
+		let options = {
+			method: req.method,
+	    	headers: req.headers,
+	    	 url: `http://${destinationUrl}${req.url}`
+		}
 
+		logStream.write('\nProxying request to: ' + destinationUrl + req.url + '\n')
+	    through(req, logStream, {autoDestroy: false})
 
+		let downstreamResponse = req.pipe(request(options))
+		logStream.write('\n' + JSON.stringify(downstreamResponse.headers) + '\n')
+		downstreamResponse.pipe(res)
+	    through(downstreamResponse, logStream, {autoDestroy: false})
+
+	}).listen(8001);
+
+}
